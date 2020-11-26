531bceb80da5866d0ec9fe5c04922b0531f504f6
Carmine Watcher <cwatcher@21-school.ru>
2020-11-26 19:16:36 +0300

    multiple files

--- main.c
+++ main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "gnl/get_next_line.h"
 
-int main(int ac, char *av[]) {
-    char *s;
-	int fd = 0;
-	if (ac > 1)
-		fd = open(av[1], O_RDONLY);
-	if (fd < 0) {
-		printf("файл не открылся");
-		return -1;
-	}
-	printf("fd = %d\n", fd);
-	int g;
+void test_gnl(int fd) {
+//	printf("fd = %d\n", fd);
+	int r;
 	do {
-	 	g = get_next_line(fd, &s); 
-	    printf("%s$\n", s);
+    	char *s;
+	 	r = get_next_line(fd, &s);
+	    printf("%d|%s$\n", r, s);
     	free(s);
-	} while(g);
+	} while(r);
+//	printf ("close == %d\n", close(fd));
+}
+int main(int ac, char *av[]) {
+	int	fd = 0;
+	int	i = 0;
+	if (ac == 1)
+		test_gnl(fd);
+	else while (++i < ac) {
+		fd = open(av[i], O_RDONLY);
+		printf("%s\n", av[i]);
+		test_gnl(fd);
+		printf("----------------------\n");
+	}
     return (0);
 }
