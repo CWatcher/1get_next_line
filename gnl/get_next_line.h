ed06a845371d4537771458c9e10fc932c46c3d9b
Carmine <CWatcher@student.21-school.ru>
2020-12-04 08:51:25 +0300

    [norm]

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,7 +1,35 @@
-#ifndef INC_2GNL_GET_NEXT_LINE_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: CWatcher <cwatcher@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/04 01:19:41 by CWatcher          #+#    #+#             */
+/*   Updated: 2020/12/04 01:20:27 by CWatcher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int get_next_line(int fd, char **line);
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#define INC_2GNL_GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+int				get_next_line(int fd, char **ln);
+
+typedef	struct	s_buffer
+{
+	char		dat[BUFFER_SIZE];
+	char		*p;
+	ssize_t		n;
+}				t_buffer;
+
+typedef	struct	s_str
+{
+	char		*p;
+	size_t		n;
+}				t_str;
 
 #endif
