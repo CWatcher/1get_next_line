ac754c901ab1f4af83cd45bc3396d9ea8cb6fb5f
Carmine <CWatcher@student.21-school.ru>
2020-12-10 01:59:32 +0300

    +bonus

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 23:46:20 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/12/09 00:37:36 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/12/09 23:06:53 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,36 @@
 
 int	get_next_line(int fd, char **ln)
 {
-	static int		f = -2;
-	static t_buffer b;
+	static t_buffer	b[FD_MAX + 1];
 	static t_str	s;
 	size_t			j;
 
 	if (!ln)
 		return (-1);
-	if (f != fd)
+	if (fd < 0 || fd > FD_MAX)
 	{
-		b.p = b.dat;
-		b.n = 0;
-		f = fd;
+		*ln = NULL;
+		return (-1);
 	}
 	s.n = 0;
 	*ln = NULL;
-	while (0 < b.n || 0 < (b.n = read(f, b.dat, BUFFER_SIZE)))
+	while (0 < b[fd].n || 0 < (b[fd].n = read(fd, b[fd].dat, BUFFER_SIZE)))
 	{
-		if (!(s.p = malloc(s.n + b.n + 1)))
+		if (!(s.p = malloc(s.n + b[fd].n + 1)))
 			return (-1);
 		j = -1;
 		while (++j < s.n)
 			s.p[j] = (*ln)[j];
 		free(*ln);
 		*ln = s.p;
-		while (b.n && *b.p++ != '\n' && b.n--)
-			s.p[s.n++] = *(b.p - 1);
+		while (b[fd].n && b[fd].dat[b[fd].i++] != '\n' && b[fd].n--)
+			s.p[s.n++] = b[fd].dat[b[fd].i - 1];
 		s.p[s.n] = '\0';
-		b.p = b.n > 1 ? b.p : b.dat;
-		if (b.n && b.n--)
+		b[fd].i = b[fd].n > 1 ? b[fd].i : 0;
+		if (b[fd].n && b[fd].n--)
 			return (1);
 	}
-	if (!*ln && !b.n && (*ln = *ln || b.n ? *ln : malloc(sizeof(**ln))))
+	if (!*ln && !b[fd].n && (*ln = *ln || b[fd].n ? *ln : malloc(sizeof(**ln))))
 		**ln = '\0';
-	return (*ln ? b.n : -1);
+	return (*ln ? b[fd].n : -1);
 }
--- gnl/get_next_line_bonus.h
+++ gnl/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: CWatcher <cwatcher@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 01:19:41 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/12/10 00:33:08 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/12/10 00:22:39 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stddef.h>
 # include <sys/types.h>
