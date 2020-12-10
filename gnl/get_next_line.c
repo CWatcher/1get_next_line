b4975da48e6b8e35bb2b63cce22197c748fe87a1
CWatcher <cwatcher@student.21-school.ru>
2020-12-10 03:21:50 +0300

    [rf] -strdup("")

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 23:46:20 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/12/09 23:06:53 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/12/10 03:14:43 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	get_next_line(int fd, char **ln)
 	}
 	s.n = 0;
 	*ln = NULL;
-	while (0 < b[fd].n || 0 < (b[fd].n = read(fd, b[fd].dat, BUFFER_SIZE)))
+	while (0 < b[fd].n || 0 <= (b[fd].n = read(fd, b[fd].dat, BUFFER_SIZE)))
 	{
 		if (!(s.p = malloc(s.n + b[fd].n + 1)))
 			return (-1);
@@ -41,11 +41,11 @@ int	get_next_line(int fd, char **ln)
 		while (b[fd].n && b[fd].dat[b[fd].i++] != '\n' && b[fd].n--)
 			s.p[s.n++] = b[fd].dat[b[fd].i - 1];
 		s.p[s.n] = '\0';
+		if (!b[fd].i)
+			return (0);
 		b[fd].i = b[fd].n > 1 ? b[fd].i : 0;
 		if (b[fd].n && b[fd].n--)
 			return (1);
 	}
-	if (!*ln && !b[fd].n && (*ln = *ln || b[fd].n ? *ln : malloc(sizeof(**ln))))
-		**ln = '\0';
 	return (*ln ? b[fd].n : -1);
 }
